a899a85584e2275918d1950a47297949f7867cb4
shaymee <acteasily@gmail.com>
2021-07-20 22:15:13 +0900

    StudentRanking class

--- src/com/shaymee/s2/ex2_1/StudentRanking.java
+++ src/com/shaymee/s2/ex2_1/StudentRanking.java
@@ -24,7 +24,7 @@ public class StudentRanking {
 public Student[] asecRanking(Student[] students) {	// 오름차순 메소드
 	for(int idx=0;idx<students.length;idx++) {
 		for(int i=idx+1;i<students.length;i++) {
-			if(students[idx].avg > students[i].avg) {    //students[i]는 그냥 배열에 담긴 객체의 주소를 나타낸다
+			if(students[idx].avg > students[i].avg) {    // students[i]는 그냥 배열에 담긴 객체의 주소를 나타낸다
 				double tmp = students[idx].avg;
 				students[idx].avg = students[i].avg;
 				students[i].avg = tmp;										
